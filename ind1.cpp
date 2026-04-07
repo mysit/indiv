54fc4724e6cec19cc5883a5520d376be0b7a21bc
mysit <89347844+mysit@users.noreply.github.com>
2026-04-07 16:13:07 +0300

    Update ind1.cpp

--- ind1.cpp
+++ ind1.cpp
@@ -1,68 +1,81 @@
+// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
 #include <iostream>
 using namespace std;
 int main()
 {
-    int n = 93; int ten=10,flagOne=0,flagThree=0,max = 0,mul = 1,two = 2; 
+    int n = 193;
+    int ten = 10, two = 2, four = 4;
+    int sum = 0, max = 0, min = 9, count = 0, result = 0;
+
     _asm {
+        // Считаем сумму цифр, max и min
         mov eax, n;
+        mov ebx, ten;
+
     l1:
         mov edx, 0;
-        div ten;
-        cmp edx, 1;
-        je l2;
-        cmp edx, 3;
-        je l3;
+        div ebx;
+
+        add sum, edx;      // сумма цифр
+
+        // ищем max
+        cmp edx, max;
+        jle l2;
+        mov max, edx;
+
     l2:
-        add flagOne, 1;
-        cmp eax, 0;
-        jne l1;
-        jmp l4;
+        // ищем min
+        cmp edx, min;
+        jge l3;
+        mov min, edx;
+
     l3:
-        add flagThree, 1;
         cmp eax, 0;
         jne l1;
-    l4:
-        cmp flagOne, 0;
-        je l7;
-        cmp flagThree, 0;
-        jne l7;
 
-        mov eax, n;
-        mov ebx, 0;
-    l5:
-        mov edx, 0;
-        div ten;
-        cmp ebx, edx;
-        jge l6;
-        mov ebx, edx;
-    l6:
-        cmp eax, 0;
-        jne l5;
-        add max, ebx;
-        jmp l10;
-    l7:
-        mov ecx, n;
-    l8:
-        mov eax, ecx;
-        mov edx, 0;
-        div ten;
-
-        mov ecx, eax;
-        mov eax, edx;
-        mov ebx, eax;
+        // проверяем чётность суммы
+        mov eax, sum;
         mov edx, 0;
         div two;
         cmp edx, 0;
-        je l9;
-        mov eax, mul;
-        imul eax, ebx;
-        mov mul, eax;
-    l9:
-        cmp ecx, 0;
+        jne l5;
+
+        // чётная сумма: max - min
+        mov eax, max;
+        sub eax, min;
+        mov result, eax;
+        jmp l7;
+
+    l5:
+        // нечётная сумма: считаем цифры, кратные 4
+        mov eax, n;
+        mov ecx, 0;        // ecx - счётчик
+
+    l6:
+        mov edx, 0;
+        div ebx;
+
+        // проверяем, делится ли цифра (edx) на 4
+        push eax;
+        mov eax, edx;
+        mov edx, 0;
+        div four;
+        cmp edx, 0;
         jne l8;
-    l10:
+        add ecx, 1;
 
+    l8:
+        pop eax;
+        cmp eax, 0;
+        jne l6;
+
+        mov count, ecx;    // сохраняем счётчик в count
+        mov result, ecx;   // result = ecx (напрямую из регистра)
+
+    l7:
     }
-    cout << flagOne << flagThree<<" "<<max<< " "<< mul;
-}
 
+    cout << result;
+}
